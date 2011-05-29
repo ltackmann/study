954e1196ed756658efbd85a997c09d7ed6186841
Lars Tackmann <ltackmann@gmail.com>
2011-05-29 14:58:25 +0200

    added initial version of screen driver that fills screen with black color

---
+++ projects/04/Fill.asm
@@ -0,0 +1,73 @@
+// I/O handling: Infinite loop that blackens the screen when any key is pressed
+
+// while(true) {
+//  row=0
+//  color=0
+//  if(key) {
+//    color = [1111 1111 1111 1111]
+//  else
+//    color = [0000 0000 0000 0000]
+//  }
+//
+//  while(row-512 < 0) {
+//    row_adr = SCREEN + row*32
+//
+//    column=0
+//    while(column-32 < 0) {
+//        adr = row_adr + column
+//        M[adr] = color 
+//        column++
+//    }
+//    row++
+//  }
+// }
+
+// 2^15 - 1 (since we use 16 bit two complement words)
+//@32767
+//D=A
+//@black
+//M=D
+
+//@white
+//M=0
+
+// set screen pointer to start of screen memory map
+@SCREEN
+D=A
+@pointer
+M=D
+
+// max number of words in screen memory mao (512 rows with 256 pixes = 512rows x 32words = 16384)  
+@16666
+D=A
+@max
+M=D
+
+(SCREEN_LOOP)
+  // if pointer-max > 0 goto END_SCREEN_LOOP
+  @pointer
+  D=M
+  @max
+  D=D-M
+  @END_SCREEN_LOOP
+  D;JGT 
+
+  //
+  @32767
+  D=A
+  @pointer
+  A=M
+  M=D
+
+  // pointer = pointer + 1
+  @pointer
+  M=M+1
+
+  // goto SCREEN_LOOP
+  @SCREEN_LOOP
+  0;JMP
+(END_SCREEN_LOOP)
+
+// infinite loop
+@END_SCREEN_LOOP
+0;JMP
