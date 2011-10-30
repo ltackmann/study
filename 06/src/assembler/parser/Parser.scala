c9c46b99914f833c786034c9a6ce0ebb1efdec11
Lars Tackmann <ltackmann@gmail.com>
2011-10-30 18:16:32 +0100

    detect empty lines and comments

--- 06/src/assembler/parser/Parser.scala
+++ 06/src/assembler/parser/Parser.scala
@@ -1,26 +1,39 @@
-package org.randompage.tecs
+package assembler.parser
 
 import java.io.File
-import scala.io.{Source=>IO}
+import scala.io._
+
+class Parser(sourceFile: File) {
+  var sourceLines: Seq[String] = Seq()
+
+  private def parse() = {
+    // read file into sequence
+    val asmSource = Source.fromFile(sourceFile)
+
+    // TODO remove empty lines
+    val commentRegex = "(^//.*)".r
+    val emptyLineRegex = "(^$)".r
+    
+    for (line <- asmSource.getLines) {
+      line match {
+        case commentRegex(comment) => println("comment: " + comment)
+        case emptyLineRegex(emptyLine) => println("got a empty line")
+        case code => println("got code" + code)
+      }
+    }
+  }
+}
 
 object Parser {
-  def main(args : Array[String]) : Unit = {
+  def main(args: Array[String]): Unit = {
     // TODO grab from main args 
     val filePath = """test/max/MaxL.asm"""
     val sourceFile = new File(filePath)
-    if(!sourceFile.exists()) {
+    if (!sourceFile.exists()) {
       throw new java.io.FileNotFoundException(filePath)
-    } 
-    
-    parse(sourceFile)
-  }
-  
-  private def parse(sourceFile: File) = {
-    // read file into sequence
-    val asmSource = IO.fromFile(sourceFile)
-    // TODO fix that fromFile does not handle new lines correctly - add to scala samples
-    // http://stackoverflow.com/questions/1284423/read-entire-file-in-scala
-    // TODO is there a scalaz thing for this
-    asmSource.foreach(println)
+    }
+
+    val parser = new Parser(sourceFile)
+    parser.parse()
   }
 }
