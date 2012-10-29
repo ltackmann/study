560a31fda4e337361fed4bfa9ec24d47557a298b
Lars Tackmann <ltackmann@gmail.com>
2012-10-29 08:51:09 +0100

    add localstorage example

---
+++ localstorage/localstorage.dart
@@ -0,0 +1,37 @@
+#import('dart:html');
+
+TextAreaElement area;
+main() {
+  document.query('#content').innerHTML =
+    'save text locally (it will still be available after you restart your browser)';
+  area = new Element.tag('textarea'); 
+  area.style.width = '300px';
+  area.style.height = '150px';
+  document.query('#content').nodes.addLast(area);
+  
+  area.value = window.localStorage.getItem('value');
+  
+  area.on.keyUp.add((event) {
+    window.localStorage.setItem('value', area.value);
+    window.localStorage.setItem('timestamp', (new Date.now()).toString());
+    print(window.localStorage.getItem('timestamp'));
+    print(window.localStorage.getItem('value'));
+
+  });
+  
+  updateLog();
+  window.setInterval(updateLog, 5000);
+}
+
+updateLog() {
+  var delta = 0;
+    Element log = document.query('#log');
+    log.nodes = [];
+    for(int i=0; i<window.localStorage.length; i++) {
+      var elm = new Element.tag("p");
+      var key = window.localStorage.key(i);
+      elm.innerHTML = "at index $i with key ${key} and value ${window.localStorage.getItem(key)}"; 
+      log.nodes.add(elm);
+    }
+    document.query('#log').innerHTML = 'last saved: ' + delta.inSeconds + 's ago';
+}
