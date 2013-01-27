c9ad24c71c8893eb4bd784bd0c41fc034cc29b36
Lars Tackmann <ltackmann@gmail.com>
2013-01-27 21:11:37 +0100

    update dependencies

--- async/async_test.dart
+++ async/async_test.dart
@@ -1,3 +1,8 @@
 // TODO http://api.dartlang.org/docs/releases/latest/dart_async.html
 // TODO async testing with unittest lib
 
+import "dart:async";
+
+main() {
+  // TODO http://api.dartlang.org/docs/releases/latest/dart_async/Future.html
+}
