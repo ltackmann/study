d77a8d745c6358745bff4c197d107a7176ceea93
Lars Tackmann <ltackmann@gmail.com>
2013-02-11 16:15:06 +0100

    add method reflection

--- mirrors/mirrors_test.dart
+++ mirrors/mirrors_test.dart
@@ -4,7 +4,7 @@ import "package:unittest/unittest.dart";
 import "mirrors_test_classes.dart";
 
 main() {
-  group("runtimeType -", () {
+  group("mirrors -", () {
     test("type name", () {
       var obj = new MyClass();
       expect(obj.runtimeType.toString(), equals("MyClass"));
@@ -16,17 +16,46 @@ main() {
     });
     
     // TODO instatiate method from Class.type can I do Class<T>
-  });
-  
-  group("class structure -", () {
     //test("generic parameter type name", () {
     //  var instanceMirror = reflect(obj);
     //  print(instanceMirror.type.typeArguments.forEach((k,v) => print("type key $k value $v")));
     //});
     
-    //  im.type.methods.values.forEach((MethodMirror method) => print(method.simpleName));
-    // TODO class methods/fields (and access level (public, final, private)) 
+    solo_test("reflect methods", () {
+      var obj = new MyClass();
+      var instanceMirror = reflect(obj);
+      
+      int passed = 0;
+      instanceMirror.type.methods.values.forEach((MethodMirror method) {
+        if(method.simpleName == "_privateMethod") {
+          _assertMethod(method, isPrivate:true, isStatic:false, returnType:"String", argTypes:[]); 
+          // TODO invoke methods
+          passed++;
+        }
+        if(method.simpleName == "_privateStaticMethod") {
+          _assertMethod(method, isPrivate:true, isStatic:true, returnType:"String", argTypes:[]); 
+          passed++;
+        }
+        if(method.simpleName == "publicMethod") {
+          _assertMethod(method, isPrivate:false, isStatic:false, returnType:"String", argTypes:["String", "int"]); 
+          passed++;    
+        }
+        if(method.simpleName == "publicStaticMethod") {
+          _assertMethod(method, isPrivate:false, isStatic:true, returnType:"String", argTypes:["String", "int"]); 
+          passed++;
+        }
+      });
+      expect(passed, equals(4));
+    });
+    
+    
+    test("reflect fields", () {
+      
+    });
+    
+    // TODO class methods/fields (and access level (public, final, private)) http://api.dartlang.org/docs/releases/latest/dart_mirrors/MethodMirror.html 
     // TODO method parameter types/numbers
+    
     // TODO access meta data
     // TODO class Type hireacy (super classes and generic arguments (get T's type from Class<T> at runtime))
   });
@@ -38,5 +67,13 @@ main() {
   // if possible
   // TODO find all versions of a loaded type 
   // TODO name of calling class (perhaps using getName(type)
+}
+
+_assertMethod(MethodMirror method, {bool isPrivate: null, bool isStatic:null, String returnType:null, List<String> argTypes: null}) {
+  expect(method.isPrivate, isPrivate);
+  expect(method.isStatic, isStatic);
+  expect(method.returnType.simpleName, equals(returnType));
+  var actualArgTypes = method.parameters.map((ParameterMirror pm) => pm.type.simpleName);
+  expect(actualArgTypes, orderedEquals(argTypes));
 }
 
--- mirrors/mirrors_test_classes.dart
+++ mirrors/mirrors_test_classes.dart
@@ -1,11 +1,38 @@
 library mirrors_test_classes;
 
-/**
- * Put classes in their own lib to see how libary structure inpacts the mirror data
- */
+import "package:meta/meta.dart";
 
+/**
+ * Test class for using reflection
+ */
 class MyClass {
+  static String publicStaticField = "public static field";
+  static String _privateStaticField = "private static field";
   
+  static String publicStaticMethod(String str, int integer) => "public static method";
+  static String _privateStaticMethod() => "private static method";
+  
+  MyClass():
+    publicField = "public field",
+    _privateField = "private field",
+    publicFinalField = "public final field",
+    _privateFinalField = "private final field";
+  
+  @override
+  String publicMethod(String str, int integer) => "public method";
+  String _privateMethod() => "private method";
+  
+  String get publicGetter => "public getter";
+  String get _privateGetter => "private getter";
+  
+  set publicSetter(String value) => publicField = value;
+  set _privateSetter(String value) => _privateField = value;
+  
+  @override
+  String publicField;
+  String _privateField;
+  final String publicFinalField;
+  final String _privateFinalField;
 }
 
 class MyGenericClass<T> {
