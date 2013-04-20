b4db8e562f53e553d2c87385ee6762dc225db922
Lars Tackmann <ltackmann@gmail.com>
2013-04-20 10:40:38 +0200

    fixed mirrors

--- install.sh
+++
@@ -1,5 +0,0 @@
-find . -iname "pubspec.lock" -exec rm -rf {} \;
-find . -iname "packages" -exec rm -rf {} \;
-
-#find . -iname "pubspec.yaml" -execdir pub install \;
-
--- mirrors/mirrors_test.dart
+++ mirrors/mirrors_test.dart
@@ -28,20 +28,20 @@ main() {
       
       int passed = 0;
       im.type.methods.values.forEach((MethodMirror method) {
-        if(method.simpleName == "_privateMethod") {
+        // TODO invoke methods http://phylotic.blogspot.dk/2012/08/working-with-mirrors-in-dart-brief.html
+        if(MirrorSystem.getName(method.simpleName) == "_privateMethod") {
           _assertMethod(method, isPrivate:true, isStatic:false, returnType:"String", argTypes:[]); 
-          // TODO invoke methods http://phylotic.blogspot.dk/2012/08/working-with-mirrors-in-dart-brief.html
           passed++;
         }
-        if(method.simpleName == "_privateStaticMethod") {
+        if(MirrorSystem.getName(method.simpleName) == "_privateStaticMethod") {
           _assertMethod(method, isPrivate:true, isStatic:true, returnType:"String", argTypes:[]); 
           passed++;
         }
-        if(method.simpleName == "publicMethod") {
+        if(method.simpleName == new Symbol("publicMethod")) {
           _assertMethod(method, isPrivate:false, isStatic:false, returnType:"String", argTypes:["String", "int"]); 
           passed++;    
         }
-        if(method.simpleName == "publicStaticMethod") {
+        if(method.simpleName == new Symbol("publicStaticMethod")) {
           _assertMethod(method, isPrivate:false, isStatic:true, returnType:"String", argTypes:["String", "int"]); 
           passed++;
         }
@@ -50,13 +50,13 @@ main() {
     });
     
     test("reflect fields", () {
-      
+      // TODO
     });
     
     test("emit instance from prototype", () {
       var im = reflect(new MyClass());
       var cm = im.type;
-      cm.newInstance('',[]).then(expectAsync1((InstanceMirror newIm) {
+      cm.newInstanceAsync(new Symbol(''),[]).then(expectAsync1((InstanceMirror newIm) {
         var instance = newIm.reflectee;
         expect(instance.publicField, equals("public field"));
       })); 
@@ -76,7 +76,7 @@ main() {
     test('qualified type name', () {
       var type = MyClass;
       var name = new Symbol(type.toString());
-      //TODO reflectClass(MyClass);
+      // TODO reflectClass(MyClass);
       var cm = currentMirrorSystem().libraries.values
           .where((lib) => lib.classes.containsKey(name))
           .map((lib) => lib.classes[name])
@@ -90,13 +90,13 @@ main() {
       var mirrorSystem = currentMirrorSystem();
       expect(mirrorSystem.libraries, isNot(isEmpty));
       
-      var lib = mirrorSystem.libraries["mirrors_test_classes"];
+      var lib = mirrorSystem.libraries[new Symbol("mirrors_test_classes")];
       expect(lib, isNotNull);
       
       expect(lib.classes.length, equals(2));
-      expect(lib.classes["MyClass"], isNotNull);
+      expect(lib.classes[new Symbol("MyClass")], isNotNull);
       expect(lib.functions.length, equals(1));
-      expect(lib.functions["myFunction"], isNotNull);
+      expect(lib.functions[new Symbol("myFunction")], isNotNull);
     });
   });
 }
@@ -104,8 +104,8 @@ main() {
 _assertMethod(MethodMirror method, {bool isPrivate: null, bool isStatic:null, String returnType:null, List<String> argTypes: null}) {
   expect(method.isPrivate, isPrivate);
   expect(method.isStatic, isStatic);
-  expect(method.returnType.simpleName, equals(returnType));
+  expect(method.returnType.simpleName, equals(new Symbol(returnType)));
   var actualArgTypes = method.parameters.map((ParameterMirror pm) => pm.type.simpleName);
-  expect(actualArgTypes, orderedEquals(argTypes));
+  expect( actualArgTypes, orderedEquals(argTypes.map((a) => new Symbol(a))) );
 }
 
