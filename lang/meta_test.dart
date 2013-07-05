7a5d95eb5cf9dadf347c1e7071f4f7849bd54a0b
Lars Tackmann <ltackmann@gmail.com>
2013-07-05 16:21:15 +0200

    better metadata example

--- lang/meta_test.dart
+++ lang/meta_test.dart
@@ -3,23 +3,44 @@ library test.metadata_test;
 import 'dart:mirrors';
 import 'package:unittest/unittest.dart';
 
-class Awesome {
-  final String msg;
-  const Awesome(this.msg);
+class Message {
+  const Message(this.msg);
   
-  String toString() => msg;
+  final String msg;
 }
 
-@Awesome('it works!')
-class Cool {
+@Message('class')
+class Person {
+  @Message('constructor')
+  Person(this.name);
   
+  @Message('field')
+  String name;
 }
 
 void main() {
   test("meta test", (){
-    var classMirror = reflectClass(Cool);
-    var metadata = classMirror.metadata;
-    var obj = metadata.first.reflectee;
-    expect(obj.toString(), equals("it works!"));
+    var classMirror = reflectClass(Person);
+   
+    // class
+    assertMetadata(classMirror.metadata, "class");
+    
+    // constructor
+    expect(classMirror.constructors.length, equals(1));
+    classMirror.constructors.forEach((s,m) {
+      assertMetadata(m.metadata, "constructor");
+    });
+    
+    // field
+    expect(classMirror.members.length, equals(1));
+    classMirror.members.forEach((s,m) {
+      assertMetadata(m.metadata, "field");
+    });
   });
+}
+
+bool assertMetadata(List<InstanceMirror> metadata, String expected) {
+  expect(metadata.length, equals(1));
+  expect(metadata.first.reflectee is Message, isTrue);
+  expect(metadata.first.reflectee.msg, equals(expected));
 }
