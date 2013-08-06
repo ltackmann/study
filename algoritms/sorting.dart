6ebc61df3dedc18f39185b03a959e579b44956a6
Lars Tackmann <ltackmann@gmail.com>
2013-08-06 11:00:20 +0200

    add selection sort

--- algoritms/sorting.dart
+++ algoritms/sorting.dart
@@ -2,8 +2,21 @@ import 'package:unittest/unittest.dart';
 
 main() {
   group("sorting algorithms -", () {
-    test("selection sort", () {
-      
+    solo_test("selection sort", () {
+      selectionSort(List<Comparable> list) {
+        final length = list.length;
+        for(int i=0; i<length-1; i++) {
+          var smallest = i;
+          for(int j=i+1; j<length; j++) {
+            if(list[j].compareTo(list[smallest]) < 0) {
+              smallest = j;
+            }
+          }
+          swap(list, smallest, i);
+        }
+        return list;
+      }
+      expect(selectionSort([6,3,1,4,2,5]), equals([1,2,3,4,5,6]));
     });
     
     test("insertion sort", () {
@@ -18,4 +31,10 @@ main() {
       
     });
   });
+}
+
+swap(List list, int i, int j) {
+  var tmp = list[i];
+  list[i] = list[j];
+  list[j] = tmp;
 }
