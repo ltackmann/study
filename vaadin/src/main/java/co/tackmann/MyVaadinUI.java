845a4aaf102492c15ef0e2cc9148e4f38af0673e
Lars Tackmann <ltackmann@gmail.com>
2013-08-07 21:03:56 +0200

    added more vaadin components

--- vaadin/src/main/java/co/tackmann/MyVaadinUI.java
+++ vaadin/src/main/java/co/tackmann/MyVaadinUI.java
@@ -9,32 +9,38 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @Theme("mytheme")
 @SuppressWarnings("serial")
-public class MyVaadinUI extends UI
-{
+public class MyVaadinUI extends UI {
+	// TODO can this be moved out ?
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "co.tackmann.AppWidgetSet")
+	public static class Servlet extends VaadinServlet {
+	}
 
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "co.tackmann.AppWidgetSet")
-    public static class Servlet extends VaadinServlet {
-    }
+	@Override
+	protected void init(VaadinRequest request) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+		
+		final TextField nameField = new TextField("Name"); 
+		layout.addComponent(nameField);
+		
+		final Label nameLabel = new Label();
+		layout.addComponent(nameLabel);
 
-    @Override
-    protected void init(VaadinRequest request) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
-        
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
-    }
+		Button button = new Button("Click Me");
+		button.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				nameLabel.setCaption("Hello " + nameField.getValue());
+			}
+		});
+		layout.addComponent(button);
+	}
 
 }
