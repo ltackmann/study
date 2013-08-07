011b805df8bfe19a30f657ce254abad57f595fa3
Lars Tackmann <ltackmann@gmail.com>
2013-08-07 22:43:45 +0200

    added merge sort

--- algoritms/sorting.dart
+++ algoritms/sorting.dart
@@ -16,10 +16,10 @@ main() {
         }
         return list;
       }
-      assertIntegerSort(selectionSort);
+      assertSort(selectionSort);
     });
     
-    solo_test("insertion sort", () {
+    test("insertion sort", () {
       List<Comparable> insertionSort(List<Comparable> list) {
         final length = list.length;
         for(int i=1; i<length; i++) {
@@ -33,11 +33,47 @@ main() {
         }
         return list;
       }
-      assertIntegerSort(insertionSort);
+      assertSort(insertionSort);
     });
     
-    test("merge sort", () {
+    solo_test("merge sort", () {
+      merge(List<Comparable> list, int start, int pivot, int end) {
+        List<Comparable> tmp1 = list.getRange(start, pivot+1).toList()..add(null); 
+        List<Comparable> tmp2 = list.getRange(pivot+1, end+1).toList()..add(null); 
+        int i = 0, j = 0;
+        for(int k=start; k<=end; k++) {
+          if(tmp1[i] != null && tmp2[j] == null) {
+            list[k] = tmp1[i];
+            i++;
+          } else if(tmp1[i] == null && tmp2[j] != null) {
+            list[k] = tmp2[j];
+            j++;
+          } else if(tmp1[i].compareTo(tmp2[j]) <= 0) {
+            list[k] = tmp1[i];
+            i++;
+          } else {
+            list[k] = tmp2[j];
+            j++;
+          }
+        }
+      }
       
+      mergeSortRec(List<Comparable> list, int start, int end) {
+        if(start >= end) {
+          return;
+        }
+        var pivot = (start + end) ~/ 2;
+        mergeSortRec(list, start, pivot);
+        mergeSortRec(list, pivot + 1, end);
+        merge(list, start, pivot, end);
+      }
+      
+      List<Comparable> mergeSort(List<Comparable> list) {
+        mergeSortRec(list, 0, list.length - 1);
+        return list;
+      }
+      
+      assertSort(mergeSort);
     });
     
     test("quick sort", () {
@@ -60,7 +96,7 @@ swap(List list, int i, int j) {
   list[j] = tmp;
 }
 
-assertIntegerSort(List<Comparable> sorter(List<Comparable> list)) {
+assertSort(List<Comparable> sorter(List<Comparable> list)) {
   expect(sorter([3,1,2,5,4]), equals([1,2,3,4,5]));
   expect(sorter([6,3,1,4,2,5]), equals([1,2,3,4,5,6]));
 }
