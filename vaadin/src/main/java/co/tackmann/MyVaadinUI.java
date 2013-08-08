aab112330177e60618df62b387cbfbee60a3fffa
Lars Tackmann <ltackmann@gmail.com>
2013-08-08 11:40:09 +0200

    structure code better and add notification

--- vaadin/src/main/java/co/tackmann/MyVaadinUI.java
+++ vaadin/src/main/java/co/tackmann/MyVaadinUI.java
@@ -9,6 +9,7 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -16,31 +17,51 @@ import com.vaadin.ui.VerticalLayout;
 @Theme("mytheme")
 @SuppressWarnings("serial")
 public class MyVaadinUI extends UI {
+	private VerticalLayout layout = new VerticalLayout();
+
+	private final TextField nameField = new TextField("Name");
+	private final Label nameLabel = new Label();
+	private Button button = new Button("Click Me");
+	private Button notificationButton = new Button("Notify Me");
+
+	// private ComboBox combo = new ComboBox(" Test");
+	// private CheckBox checkBox = new CheckBox(" Keep previous results");
+	// private Button button = new Button(" Time it!");
+	// private VerticalLayout resultsLayout = new VerticalLayout();
+
 	// TODO can this be moved out ?
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "co.tackmann.AppWidgetSet")
 	public static class Servlet extends VaadinServlet {
 	}
 
-	@Override
-	protected void init(VaadinRequest request) {
-		final VerticalLayout layout = new VerticalLayout();
+	void buildLayout() {
 		layout.setMargin(true);
 		setContent(layout);
-		
-		final TextField nameField = new TextField("Name"); 
-		layout.addComponent(nameField);
-		
-		final Label nameLabel = new Label();
-		layout.addComponent(nameLabel);
 
-		Button button = new Button("Click Me");
+		layout.addComponent(nameField);
+		layout.addComponent(nameLabel);
+		layout.addComponent(button);
+		layout.addComponent(notificationButton);
+	}
+
+	void handleEvents() {
 		button.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
 				nameLabel.setCaption("Hello " + nameField.getValue());
 			}
 		});
-		layout.addComponent(button);
+		notificationButton.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				Notification.show("Hello " + nameField.getValue());
+			}
+		});
 	}
 
+	@Override
+	protected void init(VaadinRequest request) {
+		// TODO hide/show content
+		buildLayout();
+		handleEvents();
+	}
 }
