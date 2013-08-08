8bf77e38187a54e37a1d98c7bffd1da9fd69fa66
Lars Tackmann <ltackmann@gmail.com>
2013-08-08 10:38:32 +0200

    added quick sort

--- algoritms/sorting.dart
+++ algoritms/sorting.dart
@@ -16,6 +16,7 @@ main() {
         }
         return list;
       }
+      
       assertSort(selectionSort);
     });
     
@@ -33,10 +34,13 @@ main() {
         }
         return list;
       }
+      
       assertSort(insertionSort);
     });
     
     test("merge sort", () {
+      // merging the sorted sublists list[start..pivot] and list[pivot..end] 
+      // into one sorted list list[start..end] 
       merge(List<Comparable> list, int start, int pivot, int end) {
         List<Comparable> tmp1 = list.getRange(start, pivot+1).toList(); 
         List<Comparable> tmp2 = list.getRange(pivot+1, end+1).toList(); 
@@ -76,8 +80,37 @@ main() {
       assertSort(mergeSort);
     });
     
-    test("quick sort", () {
+    solo_test("quick sort", () {
+      // rearranges list[start..end] so that every element in 
+      // list[start..pivot] is less than list[pivot] and every 
+      // element in list[pivot..end] is greater than list[pivot]
+      int partition(List<Comparable> list, int start, int end) {
+        int pivot = start;
+        for(int i=start; i<end; i++) {
+          if(list[i].compareTo(list[end]) <= 0) {
+            swap(list, pivot, i);
+            pivot++;
+          }
+        }
+        swap(list, pivot, end);
+        return pivot;
+      }
       
+      quickSortRec(List<Comparable> list, int start, int end) {
+        if(start >= end) {
+          return;
+        }
+        int pivot = partition(list, start, end);
+        quickSortRec(list, start, pivot-1);
+        quickSortRec(list, pivot+1, end);
+      }
+      
+      List<Comparable> quickSort(List<Comparable> list) {
+        quickSortRec(list, 0, list.length - 1);
+        return list;
+      }
+      
+      assertSort(quickSort);
     });
     
     test("count sort", () {
