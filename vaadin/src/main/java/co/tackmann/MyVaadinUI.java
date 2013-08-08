230f6a9e2c145853e96a8604557755930204747a
Lars Tackmann <ltackmann@gmail.com>
2013-08-08 15:34:55 +0200

    better code structure

--- vaadin/src/main/java/co/tackmann/MyVaadinUI.java
+++ vaadin/src/main/java/co/tackmann/MyVaadinUI.java
@@ -11,6 +11,7 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
@@ -21,34 +22,25 @@ import com.vaadin.ui.VerticalLayout;
 @SuppressWarnings("serial")
 public class MyVaadinUI extends UI {
 	private VerticalLayout layout = new VerticalLayout();
+	private HorizontalLayout actionLayout = new HorizontalLayout();
+	private VerticalLayout outputLayout = new VerticalLayout();
 
-	private final TextField nameField = new TextField("Name");
-	private final Label nameLabel = new Label();
-	private Button button = new Button("Click Me");
+	private final TextField nameField = new TextField("Type your name");
+	private final Label actionLabel = new Label();
+	private Button actionButton = new Button("Click Me");
 	private ComboBox combo = new ComboBox("Test");
 
 	// private CheckBox checkBox = new CheckBox(" Keep previous results");
 	// private Button button = new Button(" Time it!");
-	// private VerticalLayout resultsLayout = new VerticalLayout();
 
 	// TODO MVP style testing with mockito
 
 	@Override
 	protected void init(VaadinRequest request) {
 		// TODO hide/show content
-		initButtons();
 		initCombo();
+		initAction();
 		initLayout();
-	}
-
-	void initLayout() {
-		layout.setMargin(true);
-		setContent(layout);
-
-		layout.addComponent(nameField);
-		layout.addComponent(nameLabel);
-		layout.addComponent(combo);
-		layout.addComponent(button);
 	}
 
 	void initCombo() {
@@ -61,35 +53,54 @@ public class MyVaadinUI extends UI {
 			@Override
 			public void valueChange(ValueChangeEvent event) {
 				ActionType actionType = (ActionType) combo.getValue();
-				if(actionType != null) {
-					button.setDescription(actionType.name);
-					button.setData(actionType);
-					button.setVisible(true);
+				if (actionType != null) {
+					actionLabel.setCaption(actionType.name);
+					actionButton.setDescription(actionType.name); // tool tip
+					actionButton.setData(actionType);
+					actionLayout.setVisible(true);
 				} else {
-					button.setVisible(false);
+					actionLayout.setVisible(false);
 				}
 			}
 		});
 		combo.setImmediate(true);
 	}
 
-	void initButtons() {
-		button.addClickListener(new Button.ClickListener() {
+	void initAction() {
+		actionButton.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
-				ActionType actionType = (ActionType) button.getData();
+				ActionType actionType = (ActionType) actionButton.getData();
 				switch (actionType) {
 				case NOTIFICATION:
 					Notification.show("Hello " + nameField.getValue());
 					break;
 				case FIELD:
-					nameLabel.setCaption("Hello " + nameField.getValue());
+					actionLabel.setCaption("Hello " + nameField.getValue());
 					break;
 				default:
-					Notification.show("please select", Notification.TYPE_ERROR_MESSAGE);
+					Notification.show("please select",
+							Notification.TYPE_ERROR_MESSAGE);
 				}
 			}
 		});
-		button.setVisible(false);
+		actionLayout.setVisible(false);
+	}
+
+	void initLayout() {
+		layout.setMargin(true);
+		setContent(layout);
+
+		layout.addComponent(nameField);
+		// first show combo when name is in place
+		layout.addComponent(combo);
+		layout.addComponent(actionLayout);
+
+		actionLayout.addComponent(actionLabel);
+		actionLayout.addComponent(actionButton);
+	}
+
+	void initFields() {
+
 	}
 
 	// TODO can this be moved out ?
