c37e34784324bea18ff0819e054fce3cc650f358
Lars Tackmann <ltackmann@gmail.com>
2013-08-29 20:22:01 +0200

    fixed samples

--- lang/io_test.dart
+++ lang/io_test.dart
@@ -1,6 +1,7 @@
-import '../../../../../../Applications/dart/dart-sdk/lib/async/async.dart';
-import '../../../../../../Applications/dart/dart-sdk/lib/io/io.dart';
-import '../../../../.pub-cache/hosted/pub.dartlang.org/unittest-0.4.7+1/lib/unittest.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:unittest/unittest.dart';
 
 // TODO http://news.dartlang.org/2013/02/io-library-now-uses-streams.html
 main() {
@@ -15,26 +16,26 @@ main() {
   group("file test -", () {
     test("get current directory", () {
       var dir = Directory.current;
-      expect(dir.path, equals("/Users/lt/Projects/dart"));
+      expect(dir.path, equals("/Users/lt/Projects/dart/sandbox/lang"));
     });
     
     test("read file sync", () {
-      var file = new File('sandbox/io/pubspec.lock');  
+      var file = new File('pubspec.lock');  
       var content = file.readAsStringSync();
       expect(content, isNot(isEmpty));
     });
     
     test("read file async", () {
-      var file = new File('sandbox/io/pubspec.lock'); 
+      var file = new File('pubspec.lock'); 
       var future = file.readAsString(); // returns immediately  
       future.then(expectAsync1((String content) {
         expect(content, isNot(isEmpty));
       }));
     });
     
-    solo_test("read file as stream", () {
-      var stream = new File('/etc/passwd').openRead().transform(new StringDecoder());
-      stream.listen((String data) => print(data));
+    test("read file as stream", () {
+      var stream = new File('/etc/passwd').openRead().transform(UTF8.decoder);
+      //stream.listen((String data) => print(data));
     });
     
     test("create file", () {
@@ -42,7 +43,7 @@ main() {
     });
     
     test("write to file", () {
-      var file = new File('sandbox/io/packages/test.txt'); 
+      var file = new File('packages/test.txt'); 
       file.openSync(mode:FileMode.APPEND).writeStringSync("test ${(new DateTime.now()).toString()}\n");
       // TODO assert by reading list line of file 
     });
@@ -65,23 +66,23 @@ main() {
   });
   
   group("http test -", () {
-    test("get", () {
+    solo_test("get", () {
       var completer = new Completer<String>();
       var httpClient = new HttpClient();
-      var conn = httpClient.get("http://www.google.com", 80, "");
-      conn.then((HttpClientRequest request) {
+      var connection = httpClient.get("google.com", 80, "");
+      connection.then(expectAsync1((HttpClientRequest request) {
         // fire request.
         return request.close();
-      }).then((HttpClientResponse response) {
-        if(response.statusCode != 200) {
-          completer.completeError(new StateError("HTTP faild with ${response.statusCode}"));
-        } else {
+      })).then(expectAsync1((HttpClientResponse response) {
+        if(response.statusCode == 200) {
           response.listen((List<int> bodyData) {
             var body = new String.fromCharCodes(bodyData);
             completer.complete(body);
           });
+        } else {
+          completer.completeError(new StateError("HTTP faild with ${response.statusCode}"));
         }
-      });
+      }));
     });
   });
   
