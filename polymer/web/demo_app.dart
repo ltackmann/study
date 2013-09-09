a0d492942f74c4ae17487a9b97d58185ee1293b6
Lars Tackmann <ltackmann@gmail.com>
2013-09-09 22:53:16 +0200

    use components for navigation

--- polymer/web/demo_app.dart
+++ polymer/web/demo_app.dart
@@ -1,13 +1,36 @@
+import 'package:route/client.dart';
+
 class DemoApp {
   DemoApp() {
-    print("loaded");
     /*
      * TODO Set up routing whith named events to centralize URL handling, but on the same
      * time make sure that URLs are actually bookmarkable 
      */
+
+    print("in demo app");
+    _router.addHandler(_effectsDemoUrl, (path) => print(path));
+    _router.addHandler(_inputDemoUrl, (path) => print(path));
+    _router.addHandler(_layoutDemoUrl, (path) => print(path));
+    _router.addHandler(_notificationDemoUrl, (path) => print(path));
+    _router.addHandler(_storageDemoUrl, (path) => print(path));
+    _router.addHandler(_svgDemoUrl, (path) => print(path));
   }
+  
+  start() {
+    _router.listen();
+    _router.gotoUrl(_inputDemoUrl, [r'index.html#'], 'home');
+  }
+  
+  final _effectsDemoUrl = new UrlPattern(r'(.*)demo/effects');
+  final _inputDemoUrl = new UrlPattern(r'(.*)demo/input');
+  final _layoutDemoUrl = new UrlPattern(r'(.*)demo/layout');
+  final _notificationDemoUrl = new UrlPattern(r'(.*)demo/notification');
+  final _storageDemoUrl = new UrlPattern(r'(.*)demo/storage');
+  final _svgDemoUrl = new UrlPattern(r'(.*)demo/svg');
+  final _router = new Router();
 }
 
 main() {
-  new DemoApp();
+  var app = new DemoApp();
+  //app.start();
 }
