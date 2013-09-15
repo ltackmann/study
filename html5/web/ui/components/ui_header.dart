9c1889d230ed771827845e461e509cbea23c5517
Lars Tackmann <ltackmann@gmail.com>
2013-09-15 21:30:43 +0200

    add language selector

--- html5/web/ui/components/ui_header.dart
+++ html5/web/ui/components/ui_header.dart
@@ -2,5 +2,11 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('ui-header')
 class UiHeader extends PolymerElement with ObservableMixin {
+  // TODO switch language in entire app when default is changed
+  // TODO load language list and default from external 
   
+  @observable int selected = 1; 
+  @observable String value = '';
+  
+  final languages = ['Danish', 'English'];
 }
