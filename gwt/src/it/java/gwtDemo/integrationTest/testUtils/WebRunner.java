78e1772108752539ef3fd5a559a9faee8c4f2686
Lars Tackmann <ltackmann@gmail.com>
2014-01-06 12:55:06 +0100

    add embedded GWT runner

--- gwt/src/it/java/gwtDemo/integrationTest/testUtils/WebRunner.java
+++ gwt/src/it/java/gwtDemo/integrationTest/testUtils/WebRunner.java
@@ -1,5 +1,35 @@
 package gwtDemo.integrationTest.testUtils;
 
-public abstract class WebRunner {
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.mortbay.thread.QueuedThreadPool;
 
+public abstract class WebRunner {
+	public WebRunner() {
+		// Create an embedded Jetty server on port 8080
+        Server server = new Server(8080);
+ 
+        // If your app isn't packaged into a WAR, you can do this instead
+        WebAppContext handler = new WebAppContext();
+        handler.setResourceBase("./war");
+        handler.setDescriptor("./war/WEB-INF/web.xml");
+        handler.setContextPath("/");
+        handler.setParentLoaderPriority(true);
+ 
+        // Add it to the server
+        server.setHandler(handler);
+ 
+        // Other misc. options
+        server.setThreadPool(new QueuedThreadPool());
+ 
+        // And start it up
+        try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
+
+
