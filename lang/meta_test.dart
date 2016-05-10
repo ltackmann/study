40c904d5801b6d6c78093f364f833c645c35efc0
Lars Tackmann <ltackmann@gmail.com>
2016-05-10 18:32:07 +0200

    minor update to meta data test

--- lang/meta_test.dart
+++ lang/meta_test.dart
@@ -5,7 +5,7 @@ import 'package:test/test.dart';
 
 class Message {
   const Message(this.msg);
-  
+
   final String msg;
 }
 
@@ -13,7 +13,7 @@ class Message {
 class Person {
   @Message('constructor')
   Person(this.name);
-  
+
   @Message('field')
   String name;
 }
@@ -21,16 +21,16 @@ class Person {
 void main() {
   test("meta test", (){
     var classMirror = reflectClass(Person);
-   
+
     // class
     assertMetadata(classMirror.metadata, "class");
-    
+
     // constructor
     expect(constructors(classMirror).length, equals(1));
     constructors(classMirror).forEach((m) {
       assertMetadata(m.metadata, "constructor");
     });
-    
+
     // field
     expect(fields(classMirror).length, equals(1));
     fields(classMirror).forEach((m) {
@@ -50,5 +50,5 @@ List<DeclarationMirror> constructors(ClassMirror classMirror) {
 }
 
 List<DeclarationMirror> fields(ClassMirror classMirror) {
-  return classMirror.declarations.values.where((DeclarationMirror decl) => decl is VariableMirror).toList(); 
+  return classMirror.declarations.values.where((DeclarationMirror decl) => decl is VariableMirror).toList();
 }
