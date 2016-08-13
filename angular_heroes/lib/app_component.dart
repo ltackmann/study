262a0cd8bbbd4e4a01759e0fe5305a64acbe726d
Lars Tackmann <ltackmann@gmail.com>
2016-08-13 18:58:26 +0200

    added sub component

--- angular_heroes/lib/app_component.dart
+++ angular_heroes/lib/app_component.dart
@@ -1,23 +1,32 @@
 import 'package:angular2/core.dart';
-
-class Hero {
-  final int id;
-  String name;
-
-  Hero(this.id, this.name);
-}
+import 'hero.dart';
+import 'hero_detail_component.dart';
 
 @Component(
-    selector: 'my-app',
-    template: '''
-      <h1>{{title}}</h1>
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>''')
+  selector: 'my-app',
+  templateUrl: 'app_component.html',
+  directives: const [HeroDetailComponent])
 class AppComponent {
+  Hero selectedHero;
   String title = 'Tour of Heroes';
+  final List<Hero> heroes = mockHeroes;
+
   Hero hero = new Hero(1, 'Windstorm');
+
+  onSelect(Hero hero) {
+    selectedHero = hero;
+  }
 }
+
+final List<Hero> mockHeroes = [
+  new Hero(11, 'Mr. Nice'),
+  new Hero(12, 'Narco'),
+  new Hero(13, 'Bombasto'),
+  new Hero(14, 'Celeritas'),
+  new Hero(15, 'Magneta'),
+  new Hero(16, 'RubberMan'),
+  new Hero(17, 'Dynama'),
+  new Hero(18, 'Dr IQ'),
+  new Hero(19, 'Magma'),
+  new Hero(20, 'Tornado')
+];
