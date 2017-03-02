3b4097d133d178243a23dcf6b49c599b125afc1f
Lars Tackmann <ltackmann@gmail.com>
2017-03-02 16:10:50 +0100

    Removed unused vaadin project and dead seam project

--- seam/src/main/java/org/randompage/samples/seam/wiki/client/EmptyResultException.java
+++
@@ -1,8 +0,0 @@
-package org.randompage.samples.seam.wiki.client;
-
-@SuppressWarnings("serial")
-public class EmptyResultException extends Exception {
-	public EmptyResultException(String arg0) {
-		super(arg0);
-	}
-}
--- vaadin/app/src/main/java/co/tackmann/vaadin/service/LanguageService.java
+++
@@ -1,7 +0,0 @@
-package co.tackmann.vaadin.service;
-
-import java.util.Set;
-
-public interface LanguageService {
-	Set<String> getLanguages();
-}
