f4ac89c2909535a44bff9453516c336ecf3b742f
Lars Tackmann <ltackmann@gmail.com>
2017-03-12 13:15:19 +0100

    start making spring jdbc sample work with spring 4

--- spring-jdbc/src/test/java/spring/jdbc/SpringTest.java
+++ spring-jdbc/src/test/java/spring/jdbc/SpringTest.java
@@ -1,55 +1,40 @@
 package spring.jdbc;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
-
-import java.util.Calendar;
-import java.util.Date;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import javax.annotation.Resource;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import spring.jdbc.dao.EmployeeDao;
+import spring.jdbc.domain.Employee;
 
 /**
  * @author Lars Tackmann
  */
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/applicationContext.xml" })
-public class SpringTest {
+public class SpringTest extends AbstractTestNGSpringContextTests {
 	private static final String username = "ltackmann";
 	@Resource
-	private EmployeeManager employeeManager;
-	@Resource
-	private HolidayManager holidayManager;
+	private EmployeeDao employeeDao;
 
 	/**
 	 * Integration test that asserts that spring injection is working as
 	 * expected
 	 */
 	@Test
-	public void springIntegrationTest() {
-		// tests injection in test class
-		assertThat(employeeManager, notNullValue());
-		assertThat(holidayManager, notNullValue());
-		// test class that is itself auto wired
-		Date from = daysFromNow(1);
-		Date to = daysFromNow(3);
-		assertThat(holidayManager.registerHoliday(username, from, to),
-				notNullValue());
-		// test that holiday schedule is updated as desired
-		Employee employee = employeeManager.getEmployee(username);
-		assertThat(employee, notNullValue());
-		assertThat(holidayManager.hasHoliday(employee, daysFromNow(2)),
-				is(true));
+	public void springInjectionTest() {
+		assertThat(employeeDao, notNullValue());
+	}
+	
+	@Test(dependsOnMethods = "springInjectionTest")
+	public void noInitialDataShouldExists() {
+		Employee employee = employeeDao.find(username);
+		assertThat(employee, nullValue());
 	}
 
-	private Date daysFromNow(int days) {
-		Calendar instance = Calendar.getInstance();
-		instance.add(Calendar.DAY_OF_MONTH, days);
-		return instance.getTime();
-	}
 }
