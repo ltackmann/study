0544ec4f7fd7f165204ba5ab0b14222138d81cd8
Lars Tackmann <ltackmann@gmail.com>
2017-03-13 20:27:23 +0100

    added simple logging example

--- logging/logging_test.dart
+++ logging/logging_test.dart
@@ -1,7 +1,11 @@
 import "package:logging/logging.dart";
 
 void main() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+
   var logger = new Logger("test");
-  // TODO configure logger
   logger.info("a info message");
 }
