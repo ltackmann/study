822ade3ce25aadcfd33cda1187402b5b3d564821
Lars Tackmann <ltackmann@gmail.com>
2023-10-08 20:58:18 +0200

    added yaml to json example

--- python/yaml_sample.py
+++ python/yaml_sample.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 
 # example of inline yaml
 def inline_yaml():
@@ -18,8 +19,30 @@ def yaml_from_file():
   print(prime_service['a'])
   print(prime_service['b']['c'])
 
+def yaml_to_json():
+  yaml_string="""employee:
+    name: John Doe
+    age: 35
+    job:
+      title: Software Engineer
+      department: IT
+      years_of_experience: 10
+    address:
+      street: 123 Main St.
+      city: San Francisco
+      state: CA
+      zip: 94102
+  """
+  print("The YAML string is:")
+  print(yaml_string)
+  python_dict=yaml.safe_load(yaml_string)
+  json_string=json.dumps(python_dict)
+  print("The JSON string is:")
+  print(json_string)
+
 def run_all():
   inline_yaml()
   yaml_from_file()
+  yaml_to_json()
 
 run_all()
