345d74ad2df6a5d8d90ce3d9cff7c5fe025eecca
Lars Tackmann <ltackmann@gmail.com>
2023-11-14 23:31:34 +0100

    more samples

--- flutter/dart/records.dart
+++ flutter/dart/records.dart
@@ -1,9 +1,15 @@
 main() {
+  // record as a indexed tuple
   (String, int) nameAndAge;
   nameAndAge = ("lars", 42);
-  var name = nameAndAge.$1;
-  var age = nameAndAge.$2;
+  print("${nameAndAge.$1} is ${nameAndAge.$2} years old");
 
+  // deconstruct tuple into named values
   var (nameValue, ageValue) = nameAndAge;
   print("$nameValue is $ageValue years old");
+
+  // named record
+  ({String name, int age}) namedRecord;
+  namedRecord = (name: "Lars", age: 42);
+  print("${namedRecord.name} is ${namedRecord.age} years old");
 }
