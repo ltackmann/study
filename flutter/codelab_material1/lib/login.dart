f767ed18e675c5421bf87d0d25bc3c33bfc2c487
Lars Tackmann <ltackmann@gmail.com>
2023-12-18 21:49:26 +0100

    added text fields

--- flutter/codelab_material1/lib/login.dart
+++ flutter/codelab_material1/lib/login.dart
@@ -27,6 +27,24 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 120.0),
             // TODO: Remove filled: true values (103)
             // TODO: Add TextField widgets (101)
+            // TODO: Add TextField widgets (101)
+            // [Name]
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            // spacer
+            const SizedBox(height: 12.0),
+            // [Password]
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
             // TODO: Add button bar (101)
           ],
         ),
